0b349edff858a69ff985838567e95636c2bd70bb
JavierJSX <javier2072014@gmail.com>
2025-08-17 15:11:26 -0400

    .

--- src/components/FormView/FormView.jsx
+++ src/components/FormView/FormView.jsx
@@ -1,22 +1,41 @@
 import React from'react'
 import './FormView.css'
 
-function FormView({ name, age, colorlike, isSubmitted }){
+function FormView({ name, age, colorlike, isSubmitted, onReset }){
 
     return(
         <div className='formView'>
-            <h1>FormView</h1>
             {isSubmitted ?(
-                <>
-                <h1>Datos enviados</h1>
-                <h1> Nombre: {name} </h1>
-                <h1> Edad: {age} </h1>
-                <h1> Color Favorito{colorlike} </h1>
-                </>
+            <>
+            <div className='header'>
+                <h1>Datos Enviados Exitosamente</h1>
+            </div>
+
+            <div className='data-container'>               
+                <div className='data-item'>
+                    <span className='data-label'> Nombre:</span>
+                    <span className='data-value'>{name}</span>
+                </div>
+                
+                <div className='data-item'>
+                    <span className='data-label'> Color Favorito:</span>
+                    <span className='data-value'>{colorlike}</span>
+                </div>
+            </div>
+
+            <div className='actions'>
+                <button
+                    className='reset-button'
+                    onClick={onReset}
+                >
+                    Nuevo Formulario
+                </button>
+            </div>
+             </>
             ):(
-                <p>Complete el formulario y presiona "Eviar Formulario" para ver los datos</p>
+                 <p>Complete el formulario y presiona "Eviar Formulario" para ver los datos</p>                  
             )}
-        </div>
+        </div>        
     );
 };
 
